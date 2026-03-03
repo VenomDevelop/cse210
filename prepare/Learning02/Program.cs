0f23a85b2a83a867dbd35979fa3c8e4d89df79e3
Genghis Khan <venomdevelop@gmail.com>
2026-03-03 09:37:33 -0700

    job class working

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job myFirstJob = new Job();
+
+        myFirstJob._companyName = "Toku";
+        myFirstJob._jobTitle = "Software Engineer";
+        myFirstJob._startYear = 2019;
+        myFirstJob._endYear = 2026;
+
+        myFirstJob.DisplayJob();
+
+        Job mySecondJob = new Job();
+        mySecondJob._companyName = "Jimmy Johns";
+        mySecondJob._jobTitle = "Inshop";
+        mySecondJob._startYear = 2022;
+        mySecondJob._endYear = 2026;
+        mySecondJob.DisplayJob();
     }
 }
